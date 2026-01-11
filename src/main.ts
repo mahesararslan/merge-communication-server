44501af008d61e351a8e1162d8aeb70576611f5a
Arslan Mahesar <mahesararslan1998@gmail.com>
2026-01-11 08:18:21 +0500

    Adds cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const allowedOrigins = process.env.ALLOWED_ORIGINS 
+    ? process.env.ALLOWED_ORIGINS.split(',').map(origin => origin.trim())
+    : ['http://localhost:3000', 'https://www.mergeedu.app', 'http://localhost:5173', 'https://merge-frontend-five.vercel.app'];
+
+  app.enableCors({
+    origin: allowedOrigins,
+    credentials: true,
+  });
+
+
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
